78315b78db8eba019750028035d9a6ad9ce99506
aquacash5 <kylejbloom@gmail.com>
2016-06-20 17:35:13 -0700

    Reverted back to previous verison

--- podtube.py
+++ podtube.py
@@ -11,9 +11,7 @@ import requests
 from feedgen.feed import FeedGenerator
 from pytube import YouTube
 from tornado import gen
-from tornado import httputil
 from tornado import ioloop
-from tornado import iostream
 from tornado import process
 from tornado import web
 from tornado.locks import Semaphore
@@ -271,96 +269,18 @@ class AudioHandler(web.RequestHandler):
             while audio in conversion_queue:
                 yield gen.sleep(0.5)
         self.add_header('Content-Type', 'audio/mpeg')
-        request_range = None
-        range_header = self.request.headers.get("Range")
-        if range_header:
-            # As per RFC 2616 14.16, if an invalid Range header is specified,
-            # the request will be treated as if the header didn't exist.
-            request_range = httputil._parse_request_range(range_header)
-        size = os.stat(file).st_size
-        if request_range:
-            start, end = request_range
-            if (start is not None and start >= size) or end == 0:
-                # As per RFC 2616 14.35.1, a range is not satisfiable only: if
-                # the first requested byte is equal to or greater than the
-                # content, or when a suffix with length 0 is specified
-                self.set_status(416)  # Range Not Satisfiable
-                self.set_header("Content-Type", "text/plain")
-                self.set_header("Content-Range", "bytes */%s" % (size,))
-                return
-            if start is not None and start < 0:
-                start += size
-            if end is not None and end > size:
-                # Clients sometimes blindly use a large range to limit their
-                # download size; cap the endpoint at the actual file size.
-                end = size
-            # Note: only return HTTP 206 if less than the entire range has been
-            # requested. Not only is this semantically correct, but Chrome
-            # refuses to play audio if it gets an HTTP 206 in response to
-            # ``Range: bytes=0-``.
-            if size != (end or size) - (start or 0):
-                self.set_status(206)  # Partial Content
-                self.set_header("Content-Range", httputil._get_content_range(start, end, size))
-        else:
-            start = end = None
-        if start is not None and end is not None:
-            content_length = end - start
-        elif end is not None:
-            content_length = end
-        elif start is not None:
-            content_length = size - start
-        else:
-            content_length = size
-        self.set_header("Content-Length", content_length)
-        content = self.get_content(file, start, end)
-        if isinstance(content, bytes):
-            content = [content]
-        for chunk in content:
-            try:
-                self.write(chunk)
-                yield self.flush()
-            except iostream.StreamClosedError:
-                return
+        self.add_header('Content-Length', os.stat(file).st_size)
+        with(file, 'rb') as f:
+            while True:
+                chunk = f.read(1024 ** 2)
+                if chunk:
+                    self.write(chunk)
+                    yield self.flush()
+                else:
+                    return
 
     def on_connection_close(self):
         logging.info('Audio: User quit during transcoding (%s)', self.request.remote_ip)
-
-    @classmethod
-    def get_content(cls, abspath, start=None, end=None):
-        """Retrieve the content of the requested resource which is located
-        at the given absolute path.
-
-        This class method may be overridden by subclasses.  Note that its
-        signature is different from other overridable class methods
-        (no ``settings`` argument); this is deliberate to ensure that
-        ``abspath`` is able to stand on its own as a cache key.
-
-        This method should either return a byte string or an iterator
-        of byte strings.  The latter is preferred for large files
-        as it helps reduce memory fragmentation.
-
-        .. versionadded:: 3.1
-        """
-        with open(abspath, "rb") as file:
-            if start is not None:
-                file.seek(start)
-            if end is not None:
-                remaining = end - (start or 0)
-            else:
-                remaining = None
-            while True:
-                chunk_size = 1024 ** 2
-                if remaining is not None and remaining < chunk_size:
-                    chunk_size = remaining
-                chunk = file.read(chunk_size)
-                if chunk:
-                    if remaining is not None:
-                        remaining -= len(chunk)
-                    yield chunk
-                else:
-                    if remaining is not None:
-                        assert remaining == 0
-                    return
 
 
 class FileHandler(web.RequestHandler):
